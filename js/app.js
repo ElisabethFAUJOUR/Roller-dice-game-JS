61e286ab26efdec9177e4bd942fff7a0c2869d9a
Elisabeth Faujour <efaujour@gmail.com>
2023-05-23 17:47:20 +0200

    lancer plusieurs dés avec refacto

--- js/app.js
+++ js/app.js
@@ -1,50 +1,51 @@
-//ETAPE 1 CREATION DU DE
+// //ETAPE 1 CREATION DU DE
 
-/*Création de la div pour le dé "dice"*/
-const diceDiv = document.createElement('div'); 
+// /*Création de la div pour le dé "dice"*/
+// const diceDiv = document.createElement('div'); 
 
-/*Ajout de la class = "dice" à la div*/
-diceDiv.classList.add('dice'); 
+// /*Ajout de la class = "dice" à la div*/
+// diceDiv.classList.add('dice'); 
+
+// /*Selection de l'id #player dans le HTML*/
+// const player = document.getElementById('player'); 
+
+// /*Création de la div class = "dice"*/
+// player.appendChild(diceDiv); 
+
+
 
 /*Selection de l'id #player dans le HTML*/
 const player = document.getElementById('player'); 
 
-/*Création de la div class = "dice"*/
-player.appendChild(diceDiv); 
-
-
-//ETAPE 2 TIRER UN NOMBRE ALEATOIRE 
-
-// const randomInt = Math.floor(Math.random() * 6) + 1;
-// const position = (randomInt -1) * -100;
-// const dice = document.querySelector('.dice');
-// dice.style.backgroundPositionX = `${position}px`;
-
-// console.log(randomInt);
-// console.log(position);
-
-
-/*Ajouter une fonction pour tirer un nombre aléatoire*/
+/*Fonction pour GENERER UN NOMBRE ALEATOIRE ENTRE 1 ET 6*/
 function generateRandomInt() {
     return Math.floor(Math.random() * 6) + 1;
 }  
 
-/*Fonction qui affiche la face correspondante au nombre aléatoire*/
-function diceFace(number) {
-    const dice = document.querySelector('.dice'); /*Sélection de la div du dé*/
+/*Fonction pour CREER UN DE ALEATOIRE*/
+function addDice(number) {
+    const dice = document.createElement('div'); /*Création de la div pour le dé "dice"*/
+    dice.classList.add('dice'); /*Ajout de la class = "dice" à la div*/
     const position = (number - 1) * -100; /*Calcul du décalage horizontal en pixels*/
     dice.style.backgroundPositionX = `${position}px`; /*Modification du background*/
+    player.appendChild(dice); /*Création de la div class = "dice"*/
 }
 
-/* Fonction qui apelle les 2 autres fonctions => génère un nombre et affiche la face correspondate*/
+/* Fonction pour CREER LE NOMBRE DE DES ALEATOIRES + AFFICHER LES DES*/
 function rollDice() {
-    const randomInt = generateRandomInt(); /*Génération du nombre aléatoire*/
-    diceFace(randomInt); /*Affichage de la face correspondante*/
+    const numberOfDice = parseInt(prompt(`Combien de dés voulez-vous lancer ?`));
+    if (isNaN(numberOfDice) || numberOfDice <= 0) {
+        alert('Veuillez saisir un nombre valide de dés (1 et +).');
+        return;
+    }
+
+    for (let i = 0; i < numberOfDice; i++) {
+        const randomInt = generateRandomInt();/*Génération du nombre aléatoire*/
+        addDice(randomInt);/*Creation du dé en fonction du nombre aléatoire*/
+      }
 }
 
-/*Appel de la fcontion pour afficher une face au chargement de la page*/
+/*Appel de la fontion pour afficher une face au chargement de la page*/
 rollDice(); 
-
-
 
 
